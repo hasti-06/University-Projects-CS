db3a5e95c4c82e938400f5d41284e870da21fb0e
hasti-06 <hstyhashmy796@gmail.com>
2025-10-08 12:21:00 +0330

    Add files via upload

---
+++ CentigradeToFahrenheit/CentigradeToFahrenheit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentigradeToFahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the temperature in Centigrade: ");
+            double centigrade = Convert.ToDouble(Console.ReadLine());
+            double fahrenheit = centigrade * (9.0 / 5) + 32;
+            Console.WriteLine($"The temperature in Fahrenheit is: {fahrenheit}");
+        }
+    }
+}
