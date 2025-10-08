d43251db19025360102012a2bb3ea0ec64e46e62
hasti-06 <hstyhashmy796@gmail.com>
2025-10-08 12:21:30 +0330

    Add files via upload

---
+++ CircleCalculator/CircleCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the radius of the circle: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double circumference = 2 * 3.14 * radius;
+            double area = 3.14 * radius * radius;
+            Console.WriteLine($"The circumference of the circle is: {circumference}");
+            Console.WriteLine($"The area of the circle is: {area}");
+        }
+    }
+}
