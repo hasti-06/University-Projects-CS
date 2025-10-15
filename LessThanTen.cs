387a1c3eb02f10acac93f6b6db2a18f7ae8cd5d3
hasti-06 <hstyhashmy796@gmail.com>
2025-10-15 11:27:43 +0330

    Add files via upload

---
+++ LessThanTen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessThanTen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int numb = int.Parse(Console.ReadLine());
+            if (numb < 10 && numb % 2 == 0)
+            {
+                Console.WriteLine("TRUE!");
+            }
+        }
+    }
+}
---
+++ SevenOrTriple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenOrTriple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 7 == 0)
+            {
+                Console.WriteLine("YES!");
+            }
+            else
+            {
+                Console.WriteLine(num * 3);
+            }
+
+        }
+    }
+}
