6fe0f16ae98fb78f1285a5bc98b097b2db6b66f7
hasti-06 <hstyhashmy796@gmail.com>
2025-10-28 18:13:24 +0330

    Add files via upload

---
+++ EvenOddCounter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOddCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[10];
+            int odd = 0;
+            int even = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter number{0} :", i + 1);
+                num[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] % 2 == 0)
+                {
+                    even++;
+                }
+                else
+                {
+                    odd++;
+                }
+            }
+            Console.WriteLine($"You entered {even} even numbers, and {odd} odd numbers.");
+            Console.ReadKey();
+                
+
+        }
+    }
+}
---
+++ FindNum2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindNum2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[5];
+            bool found = false;
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write($"Enter number {i + 1} :");
+                num[i] = Int32.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] == 2)
+                {
+                    Console.WriteLine($"Number 2 was the {i + 1}th number you entered.");
+                    found = true;
+                    break;
+                }
+            }
+                if (!found)
+                {
+                    Console.WriteLine("You didn't enter the number 2!");
+                }
+            
+        }
+    }
+}
