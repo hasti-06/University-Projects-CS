66977e2ba0e2bcdc1e93b930a224e6fec4efe23c
hasti-06 <hstyhashmy796@gmail.com>
2025-11-09 21:30:39 +0330

    Add files via upload

---
+++ RecursiveMultiply.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveMultiply
+{
+    internal class Program
+    {
+        public static int multiply(int a, int b) {
+            if (b == 0) return 0;
+            else return a + multiply(a, b - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result = multiply(num1, num2);
+            Console.WriteLine($"{num1} * {num2} = {result}");
+        }
+    }
+}
---
+++ TriangleCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleCalculator
+{
+    internal class Program
+    {
+        public static double Perimeter(double a, double b ,double c) {
+            return  a + b + c;
+        }
+
+        public static double Area(double baselength, double height) {
+            return (baselength * height) / 2;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Triangle Perimeter: ");
+            Console.WriteLine("Enter side 1: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter side 2: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter side 3: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nTriangle Area: ");
+            Console.WriteLine("Enter base: ");
+            double baselength = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double p = Perimeter (a, b, c);
+            double area = Area(baselength, height);
+
+            Console.WriteLine("\nPerimeter of the triangle : " + p);
+            Console.WriteLine("Area of the triangle : " + area);
+        }
+    }
+}
